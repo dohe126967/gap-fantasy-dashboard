e8f37866148d98984465175655021d2732855b7f
dohe126967 <jdoherty@generalairproducts.com>
2025-08-29 09:55:19 -0400

    Update espn-proxy.js

--- netlify/functions/espn-proxy.js
+++ netlify/functions/espn-proxy.js
@@ -1,1 +1,35 @@
+export async function handler(event, context) {
+  const leagueId = event.queryStringParameters.leagueId;
+  const season = event.queryStringParameters.season || new Date().getFullYear();
 
+  const url = `https://fantasy.espn.com/apis/v3/games/ffl/seasons/${season}/segments/0/leagues/${leagueId}?view=mTeam&view=mMatchupScore&view=mStandings`;
+
+  const headers = {
+    "Cookie": `espn_s2=${process.env.ESPN_S2}; SWID=${process.env.SWID}`,
+    "User-Agent": "Mozilla/5.0",
+    "X-Fantasy-Filter": JSON.stringify({}),
+  };
+
+  try {
+    const response = await fetch(url, { headers });
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Failed to fetch data from ESPN",
+        details: err.message,
+        debug: err.stack
+      })
+    };
+  }
+}
